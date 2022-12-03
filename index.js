5bdfdbbdb030be60ad8252e040d3ad80a43e5ba1
EdArWar <edgar.vard92@gmail.com>
2022-12-03 13:13:01 +0400

    add socket io

--- index.js
+++ index.js
@@ -12,6 +12,21 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3003;
 
 const app = express();
+
+//socket
+
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(fileUpload({ useTempFiles: true }));
@@ -24,13 +39,14 @@ app.use("/userExternalAuth", externalAuthRouter);
 app.use("/product", productRouter);
 mongoose.set("useCreateIndex", true);
 mongoose.set("useFindAndModify", false);
+
 async function startApp() {
   try {
     await mongoose.connect(config.get("dbUrl"), {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`Server is Started on ${PORT} PORT`);
     });
   } catch (e) {
